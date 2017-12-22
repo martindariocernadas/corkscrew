058280e97b23ea7910ca23fe728b636f3e1c2b8a
Martin Dario Cernadas <martindario.cernadas@bbva.com>
2017-12-22 19:08:49 -0300

    make for ubuntu

---
+++ config.h
@@ -0,0 +1,39 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.in.  Generated automatically from configure.in by autoheader.  */
+
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* Define if you can safely include both <sys/time.h> and <time.h>.  */
+#define TIME_WITH_SYS_TIME 1
+
+/* Define if you have the select function.  */
+#define HAVE_SELECT 1
+
+/* Define if you have the socket function.  */
+#define HAVE_SOCKET 1
+
+/* Define if you have the strerror function.  */
+#define HAVE_STRERROR 1
+
+/* Define if you have the <string.h> header file.  */
+/* #undef HAVE_STRING_H */
+
+/* Define if you have the <sys/filio.h> header file.  */
+/* #undef HAVE_SYS_FILIO_H */
+
+/* Define if you have the xnet library (-lxnet).  */
+/* #undef HAVE_LIBXNET */
+
+/* Name of package */
+#define PACKAGE "corkscrew"
+
+/* Version number of package */
+#define VERSION "2.0"
+
+/* Define if compiler has function prototypes */
+#define PROTOTYPES 1
+
